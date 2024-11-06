240135f8d6e66f22f665f83b3ea63717ece8836a
Lavent Darvin <lavent.darvin@student.uts.edu.au>
2024-11-06 15:56:12 +1100

    WORKING CODE CHECKPOINT - SLIDE 40242

--- chipin/views.py
+++ chipin/views.py
@@ -1,14 +1,26 @@
-from django.http import HttpResponse 
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.core.mail import send_mail
-from django.urls import reverse
-from django.conf import settings
-from django.contrib.auth.models import User
 from .forms import GroupCreationForm
 from .models import Group
-import urllib.parse
+from django.contrib.auth.models import User
+
+def home(request):
+    return render(request, "chipin/home.html")
+
+@login_required
+def create_group(request):
+    if request.method == 'POST':
+        form = GroupCreationForm(request.POST, user=request.user)
+        if form.is_valid():
+            group = form.save()
+            messages.success(request, f'Group "{group.name}" created successfully!')
+            return redirect('chipin:group_detail', group_id=group.id)
+    else:
+        form = GroupCreationForm(user=request.user)  # Properly instantiate the form
+    return render(request, 'chipin/create_group.html', {'form': form})
+
 @login_required
 def group_detail(request, group_id):
     group = get_object_or_404(Group, id=group_id)
@@ -25,28 +37,44 @@ def delete_group(request, group_id):
     return redirect('chipin:home')
 
 @login_required
+def accept_invite(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    user_id = request.GET.get('user_id')
+    
+    if user_id:
+        invited_user = get_object_or_404(User, id=user_id)
+        
+        if invited_user in group.members.all():
+            messages.info(request, f'{invited_user.username} is already a member of the group "{group.name}".')
+        elif invited_user in group.invited_users.all():
+            group.members.add(invited_user)  # Add the invited user to members
+            group.invited_users.remove(invited_user)  # Remove from invited list
+            messages.success(request, f'{invited_user.username} has successfully joined the group "{group.name}".')
+        else:
+            messages.error(request, "You are not invited to join this group.")
+    else:
+        messages.error(request, "Invalid invitation link.")
+    
+    return redirect('chipin:group_detail', group_id=group.id)
+
+@login_required
 def invite_users(request, group_id):
     group = get_object_or_404(Group, id=group_id)
     users_not_in_group = User.objects.exclude(id__in=group.members.values_list('id', flat=True))
+    
     if request.method == 'POST':
-        email = request.POST.get('email')
-        send_invitation_email(group, email, request)
-        messages.success(request, f'Invitation sent to {email}')
+        user_id = request.POST.get('user_id')
+        invited_user = get_object_or_404(User, id=user_id)
+
+        if invited_user in group.invited_users.all():
+            messages.info(request, f'{invited_user.username} has already been invited.')
+        else:
+            group.invited_users.add(invited_user)
+            messages.success(request, f'Invitation sent to {invited_user.username}.')
+        
         return redirect('chipin:group_detail', group_id=group.id)
-    return render(request, 'chipin/invite_users.html', {'group': group, 'users_not_in_group': users_not_in_group})
-
-def home(request):
-    return render(request, "chipin/home.html")
-
-@login_required
-def create_group(request):
-    if request.method == 'POST':
-        form = GroupCreationForm(request.POST, user=request.user)
-        if form.is_valid():
-            group = form.save()
-            messages.success(request, f'Group "{group.name}" created successfully!')
-            return redirect('group_detail', group_id=group.id)
-    else:
-        form = GroupCreationForm(user=request.user)
-    return render(request, 'chipin/create_group.html', {'form': form})
-
+    
+    return render(request, 'chipin/invite_users.html', {
+        'group': group,
+        'users_not_in_group': users_not_in_group
+    })
